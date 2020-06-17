9edea111dd73234ddd538f439d3995e8e1af72e2
cmd410 <stresspassing@gmail.com>
2020-06-17 18:39:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# CollBool
+CollBool is an addon for [Blender](blender.org) that allows you to do auto booleans with collections!
+
+## Why?
+
+Because you might want to apply a lot of booleans on one object. For example add a lot of windows onto your virtual building, but adding them all by hand is tedious. You can, of course, unite all windows into one mesh and do a single boolean, but it takes away a lot of flexibility with editing them.
+
+Luckily, with CollBool adding large quantities of booleans is as easy as copying an object!
+
+![](./imgs/demo.gif)
+
+## Installation
+
+1. Download latest release from [releases page](https://github.com/cmd410/CollBool/releases)
+2. Open Blender 2.8 or higher
+3. Navigate to `edit -> preferences -> addons`
+4. Click `Install...` button and choose the downloaded zip archive
+5. Check `CollBool` addon in the list
+6. Enjoy!
+
+## Usage
+
+1. Create a collection for your boolean objects
+2. Select object you want to use booleans on
+3. Go into `Object properties -> Collbool` tab
+4. Check `Use Collection Boolean` and in appeared option choose a collection with your boolean objects ![](./imgs/where.png)
+5. Now any mesh that you add inside this collection will do a boolean operation on your mesh
---
+++ __init__.py
@@ -0,0 +1,164 @@
+import bpy
+
+bl_info = {
+    "name": "CollBool",
+    "description": "Addon that allows to do auto booleans with collections",
+    "author": "Crystal Melting Dot",
+    "version": (1, 0, 0),
+    "blender": (2, 80, 0),
+    "category": "Mesh",
+    "location": "Object properties -> CollBool"
+}
+
+
+def validate_collection(self, coll):
+    obj = bpy.context.object
+    coll_contents = coll.objects
+    if obj.name in coll_contents or \
+        any([
+            obj.collbool_settings.diff == coll,
+            obj.collbool_settings.unio == coll,
+            obj.collbool_settings.inte == coll
+            ]):
+        return False
+    return True
+
+
+def usage_update(self, context):
+    if self.use: return
+    modifiers = context.object.modifiers
+    for i in modifiers:
+        if i.type == 'BOOLEAN' and i.name.startswith('collbool_'):
+            modifiers.remove(i)
+
+
+def unique_mod(obj, target, name, coll):
+    modifires = obj.modifiers
+    for mod in modifires.values():
+        if mod.type != 'BOOLEAN':
+            continue
+        if not mod.object:
+            modifires.remove(mod)
+            continue
+        if mod.name.startswith('collbool_') and mod.object.name not in coll.all_objects:
+            mod.object.display_type = 'TEXTURED'
+            mod.object.hide_render = False
+            modifires.remove(mod)
+            continue
+        if mod.object == target:
+            if mod.name != name:
+                mod.name = name
+            return mod
+    mod = modifires.new(name, 'BOOLEAN')
+    mod.show_expanded = False
+    mod.object = target
+    return mod
+
+
+def handle_collbool(obj, coll, operation):
+    if not all([obj, coll, operation]): 
+        return
+    targets = [i for i in coll.all_objects if i.type == 'MESH']
+    for t in targets:
+        if t.collbool_settings.use:
+            t.collbool_settings.use = False
+        t.display_type = 'BOUNDS'
+        t.hide_render = True
+        modifier = unique_mod(obj, t, f'collbool_{operation[:4].lower()}_{t.name}_{coll.name}', coll)
+        modifier.operation = operation
+
+
+@bpy.app.handlers.persistent
+def scene_update(scene):
+    for obj in scene.objects:
+        if obj.type != 'MESH': continue
+        if not obj.collbool_settings.use: continue
+
+        if obj.collbool_settings.diff:
+            handle_collbool(obj, obj.collbool_settings.diff, 'DIFFERENCE')
+        if obj.collbool_settings.unio:
+            handle_collbool(obj, obj.collbool_settings.unio, 'UNION')
+        if obj.collbool_settings.inte:
+            handle_collbool(obj, obj.collbool_settings.inte, 'INTERSECT')
+        
+        names = [i.name for i in [
+            obj.collbool_settings.diff,
+            obj.collbool_settings.unio,
+            obj.collbool_settings.inte] if i]
+        for mod in obj.modifiers:
+            if not mod.name.startswith('collbool_'):
+                continue
+            if any([mod.name.endswith(f'_{name}') for name in names]):
+                continue
+            obj.modifiers.remove(mod)
+
+
+class CollBoolSettings(bpy.types.PropertyGroup):
+    use: bpy.props.BoolProperty(
+        name='Use Collection Boolean',
+        default=False,
+        update=usage_update
+    )
+    diff: bpy.props.PointerProperty(
+        name='Difference',
+        type=bpy.types.Collection,
+        description='Collection to apply as difference boolean',
+        poll=validate_collection)
+    unio: bpy.props.PointerProperty(
+        name='Union',
+        type=bpy.types.Collection,
+        description='Collection to apply as union boolean',
+        poll=validate_collection)
+    inte: bpy.props.PointerProperty(
+        name='Intersection',
+        type=bpy.types.Collection,
+        description='Collection to apply as intersection boolean',
+        poll=validate_collection)
+
+
+class ObjectSelectPanel(bpy.types.Panel):
+    bl_idname = "OBJECT_PT_collbool"
+    bl_label = "Collbool"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object is not None and context.object.type == 'MESH'
+
+    def draw(self, context):
+        settings = context.object.collbool_settings
+        layout = self.layout
+        layout.prop(settings, 'use')
+        if settings.use:
+            layout.prop(settings, 'diff')
+            layout.prop(settings, 'unio')
+            layout.prop(settings, 'inte')
+
+
+classes = [
+    CollBoolSettings,
+    ObjectSelectPanel
+]
+
+
+def register():
+    for c in classes:
+        bpy.utils.register_class(c)
+    bpy.types.Object.collbool_settings = bpy.props.PointerProperty(type=CollBoolSettings)
+    bpy.app.handlers.depsgraph_update_post.append(scene_update)
+
+
+def unregister():
+    for c in classes:
+        bpy.utils.unregister_class(c)
+    try:
+        bpy.app.handlers.depsgraph_update_post.remove(scene_update)
+    except ValueError:
+        pass
+
+
+if __name__ == '__main__':
+    register()
